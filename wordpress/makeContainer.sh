98a57ee545a184fe2691a980b200c6bc6c38042c
Evert Ramos <evert.ramos@gmail.com>
2016-07-24 17:02:48 -0300

    Wordpress docker

---
+++ wordpress/makeContainer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source .config
+
+docker stop $NAME
+docker rm $NAME
+
+docker run --name $NAME \
+	-v $LOCAL_FOLDER/$NAME:$CONTAINER_FOLDER \
+	--link $DB_SERVER_NAME:mysql \
+	-e WORDPRESS_DB_USER=$MYSQL_USER \
+	-e WORDPRESS_DB_PASSWORD=$MYSQL_PASSWORD \
+	-e WORDPRESS_DB_NAME=$MYSQL_DATABASE \
+	-p 8080:80 \
+	-d $CONTAINER
+
+docker ps
+
+exit 0
