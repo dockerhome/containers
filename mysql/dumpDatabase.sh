1d77d955219e87d796a6dd4b4f7a7f776538d2d7
Evert Ramos <evert.ramos@gmail.com>
2016-07-24 18:26:03 -0400

    Add dump fullfile path

--- mysql/dumpDatabase.sh
+++ mysql/dumpDatabase.sh
@@ -5,6 +5,9 @@ source .config
 [ ! -d $DUMP_PATH ] && mkdir -p $DUMP_PATH
 
 docker exec $NAME \
-	 sh -c 'exec mysqldump $MYSQL_DATABASE -uroot -p"$MYSQL_ROOT_PASSWORD"' > $DUMP_FILE
+	 sh -c 'exec mysqldump $MYSQL_DATABASE -uroot -p"$MYSQL_ROOT_PASSWORD"' > $DUMP_FULLFILE
+
+echo 'Database "$MYSQL_DATABASE" dumped to:'
+echo $DUMP_FULLFILE
 
 exit 0
--- mysql/restoreDatabase.sh
+++ mysql/restoreDatabase.sh
@@ -3,6 +3,6 @@
 source .config
 
 docker exec -i $NAME \
-	mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $DUMP_FILE
+	mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $DUMP_FULLFILE
 
 exit 0
