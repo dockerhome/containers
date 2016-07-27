ff8a339bc78110f15962962b10a525077904df93
Evert Ramos <evert.ramos@gmail.com>
2016-07-27 05:51:35 -0400

    Melhoramento no output do dumpfile

--- mysql/dumpDatabase.sh
+++ mysql/dumpDatabase.sh
@@ -7,7 +7,13 @@ source .config
 docker exec $NAME \
 	 sh -c 'exec mysqldump $MYSQL_DATABASE -uroot -p"$MYSQL_ROOT_PASSWORD"' > $DUMP_FULLFILE
 
-echo 'Database "$MYSQL_DATABASE" dumped to:'
+echo
+echo "#########################################"
+echo 
+echo "Database $MYSQL_DATABASE dumped to:"
 echo $DUMP_FULLFILE
+echo 
+echo "############# end of message ############"
+echo
 
 exit 0
