b01f1996dbfad99795165e7f56f3330a094020d0
Evert Ramos <evert.ramos@gmail.com>
2016-11-27 07:43:34 -0200

    Elasticsearch 5.0

--- elastic/makeContainer.sh
+++ elastic/makeContainer.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-source .config
+# Check if any .config option was sent
+if [ $1 ]; then
+    source $1
+else 
+    source .config
+fi
 
 docker stop $NAME
 docker rm $NAME
 
+#docker run -d --read-only --name $NAME \
 docker run -d --name $NAME \
 	-v $LOCAL_FOLDER/$NAME:$CONTAINER_FOLDER \
 	-p $LOCAL_IP:$LOCAL_PORT:$CONTAINER_PORT \
 	-p $LOCAL_IP:$LOCAL_PORT_2:$CONTAINER_PORT_2 \
 	-e ES_JAVA_OPTS="-Xms1g -Xmx1g" \
-	-P $CONTAINER
-	-D node.name=$NODE_NAME
-#	-Des.node.name=$NODE_NAME
+	-P $CONTAINER \
+	-E node.name=$NODE_NAME
 
-#docker exec
 
 docker ps
-#docker ps -a
-
-# @TODO
-#
-# 1. Colocar IP desse contianer no arquivo hosts /etc/hosts com o nome do container
-#
-# 2. Definir função inicial para ler um arquivos como parametro --config=.config_xyz ou o padrão '.config'
-#
-#
 
 exit 0
