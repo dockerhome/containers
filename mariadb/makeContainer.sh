4c572f3e20ad98496ab402238e28d25750fe0927
Evert Ramos <evert.ramos@gmail.com>
2016-12-09 06:31:56 -0500

    mariadb

--- mariadb/makeContainer.sh
+++ mariadb/makeContainer.sh
@@ -7,13 +7,15 @@ docker rm $NAME
 
 docker run --name $NAME \
 	-v $LOCAL_FOLDER/$NAME:$CONTAINER_FOLDER \
+	-v $CONF_LOCAL:$CONF_SERVER \
+	-p $LOCAL_IP:$LOCAL_PORT:$CONTAINER_PORT \
 	-e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
 	-e MYSQL_DATABASE=$MYSQL_DATABASE \
 	-e MYSQL_USER=$MYSQL_USER \
 	-e MYSQL_PASSWORD=$MYSQL_PASSWORD \
 	-d -P $CONTAINER \
-	--character-set-server=utf8 \
-	--collation-server=utf8_general_ci
+	--character-set-server=$CHARSET \
+	--collation-server=$COLLATION
 
 docker ps
 
