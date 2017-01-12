3798688a6376702a48608319f8a8882971e39e39
Evert <evert.ramos@gmail.com>
2017-01-12 10:37:45 -0500

    remove old bash

--- nginx/conf/make_pass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# required: apache2-utils
-#
-# sudo apt-get install apache2-utils
-
-sudo htpasswd -c /etc/nginx/.file_name user_name
-
-# sudo htpasswd -c /etc/nginx/.htpasswd nginx
